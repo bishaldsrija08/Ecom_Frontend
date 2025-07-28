83b2eca1fcaaeafbbe287768f13998a9e0317440
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-28 08:53:44 +0545

    feat: Add authentication check in Home component

--- src/config/handlePostOperation.js
+++ src/config/handlePostOperation.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const handlePostOperation = async (url, data) => {
     try {
-        const response = await axios.post(`http://localhost:3000/api/${url}`, data);
+        const response = await axios.post(`http://localhost:3000/api/${url}`, data, {
+            withCredentials: true
+        });
         return response;
     } catch (error) {
         return error;
--- src/pages/auth/Home.jsx
+++ src/pages/auth/Home.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate()
+  const authToken = localStorage.getItem("authToken")
+  useEffect(()=>{
+    if(!authToken){
+      navigate("/login")
+    }
+  })
   return (
     <>
         <div>Home</div>;
